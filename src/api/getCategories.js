3293f250e36b259d06b717eab9baddd20a8d833a
unknown <kaziFahmid>
2024-06-28 02:37:27 +0600

    categories api revalidate 30 sec done

--- src/api/getCategories.js
+++ src/api/getCategories.js
@@ -2,7 +2,7 @@
 
 export const getCategories = async () => {
   try {
-    const response = await fetch('https://6valley.6amtech.com/api/v1/categories?guest_id=1');
+    const response = await fetch('https://6valley.6amtech.com/api/v1/categories?guest_id=1', { cache: 'force-cache',next: { revalidate: 30000,} });
     
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
